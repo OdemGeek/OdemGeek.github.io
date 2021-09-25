f231ef59687c23fe4d9633cb28f9a9a5e2395ea6
OdemTut <ivaro5@yandex.ru>
2021-09-25 15:38:58 +0300

    Blocks is now clickable. Added script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+//set onclick event to blocks
+window.onload = function () {
+    var anchors = document.getElementsByClassName('sBlock');
+    for (var i = 0; i < anchors.length; i++) {
+        var anchor = anchors[i];
+        anchor.onclick = function () {
+            //alert('Hey, button is work!');
+            //this.setAttribute("style", "height:500px");
+            this.
+            //this.emit('eventName');
+        }
+    }
+}
