cadc4f119e7437482fb04ba32e60095dfbca0909
OdemTut <ivaro5@yandex.ru>
2022-02-06 17:44:24 +0300

    Experiment 2

--- script.js
+++ script.js
@@ -44,3 +44,32 @@ window.addEventListener('scroll',function (e) {
     }
 });
 
+
+
+let constrain = 20;
+let mouseOverContainer = document.getElementsByClassName("sBlocks");
+let ex1Layer = document.getElementsByClassName("sBlock");
+
+function transforms(x, y, el) {
+    let box = el.getBoundingClientRect();
+    let calcX = -(y - box.y - (box.height / 2)) / constrain;
+    let calcY = (x - box.x - (box.width / 2)) / constrain;
+
+    return "perspective(100px) "
+        + "   rotateX(" + calcX + "deg) "
+        + "   rotateY(" + calcY + "deg) ";
+};
+
+function transformElement(el, xyEl) {
+    el.style.transform = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer.onmousemove = function (e) {
+    let xy = [e.clientX, e.clientY];
+    let position = xy.concat([ex1Layer]);
+
+    window.requestAnimationFrame(function () {
+        transformElement(ex1Layer, position);
+    });
+    alert(position);
+};
