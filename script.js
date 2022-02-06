d5d865f56f89cac0710b71eb3371e1fe185df487
OdemTut <ivaro5@yandex.ru>
2022-02-06 19:01:41 +0300

    Added "about me"

--- script.js
+++ script.js
@@ -7,11 +7,32 @@ function createRipple(event) {
     circle.style.width = circle.style.height = `${diameter}px`;
     circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
     circle.style.top = `${event.clientY - button.offsetTop - button.dataset.yoffset - radius + window.scrollY}px`;
-    /*circle.style.left = `${event.clientX - radius}px`;
-    circle.style.top = `${event.clientY - radius}px`;*/
+
     circle.classList.add("ripple");
 
     const ripple = button.getElementsByClassName("ripple")[0];
+
+    if (ripple) {
+        ripple.remove();
+    }
+
+    button.appendChild(circle);
+
+}
+
+function createRippleOut(event) {
+    const button = event.currentTarget;
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+    circle.style.top = `${event.clientY - button.offsetTop - button.dataset.yoffset - radius + window.scrollY}px`;
+
+    circle.classList.add("rippleOut");
+
+    const ripple = button.getElementsByClassName("rippleOut")[0];
 
     if (ripple) {
         ripple.remove();
@@ -25,8 +46,8 @@ window.onload = function () {
     var buttons = document.getElementsByClassName('sBlock');
     for (const button of buttons) {
         button.addEventListener("click", createRipple);
+        //button.addEventListener("focusout", createRippleOut);
     }
-    
 }
 
 window.addEventListener('scroll',function (e) {
@@ -43,33 +64,3 @@ window.addEventListener('scroll',function (e) {
         el.className = 'helpPanelShown';
     }
 });
-
-
-
-let constrain = 20;
-let mouseOverContainer = document.getElementsByClassName("sBlocks");
-let ex1Layer = document.getElementsByClassName("sBlock");
-
-function transforms(x, y, el) {
-    let box = el.getBoundingClientRect();
-    let calcX = -(y - box.y - (box.height / 2)) / constrain;
-    let calcY = (x - box.x - (box.width / 2)) / constrain;
-
-    return "perspective(100px) "
-        + "   rotateX(" + calcX + "deg) "
-        + "   rotateY(" + calcY + "deg) ";
-};
-
-function transformElement(el, xyEl) {
-    el.style.transform = transforms.apply(null, xyEl);
-}
-
-mouseOverContainer.onmousemove = function (e) {
-    let xy = [e.clientX, e.clientY];
-    let position = xy.concat([ex1Layer]);
-
-    window.requestAnimationFrame(function () {
-        transformElement(ex1Layer, position);
-    });
-    alert(position);
-};
